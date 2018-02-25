3b237d45cc46c33defd11144f0fb254d808ff36e
dcb9 <bob@phpor.me>
2018-02-25 09:48:11 +0800

    clean code

--- main.go
+++ main.go
@@ -11,17 +11,7 @@ import (
 
 	"github.com/dghubble/oauth1"
 	twitterOAuth1 "github.com/dghubble/oauth1/twitter"
-	"github.com/dghubble/sessions"
 )
-
-const (
-	sessionName    = "example-twtter-app"
-	sessionSecret  = "example cookie signing secret"
-	sessionUserKey = "twitterID"
-)
-
-// sessionStore encodes and decodes session data stored in signed cookies
-var sessionStore = sessions.NewCookieStore([]byte(sessionSecret), nil)
 
 // Config configures the main ServeMux.
 type Config struct {
